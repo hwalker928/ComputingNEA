932cdfe2ca1341d9e3a23bd2d5515a8c3203b054
hwalker928 <harry@harryw.link>
2024-09-27 15:27:35 +0100

    start of login page

--- testing.py
+++ testing.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def root():
+    # TODO: check if the user is logged in before redirecting to login page
+    return redirect('/login')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
