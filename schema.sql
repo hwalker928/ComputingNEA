0a1f173ed766102c7c0e938a89c6ebf4731d3799
hwalker928 <harry@harryw.link>
2024-10-07 15:05:06 +0100

    start of db

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `credentials` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`last_used_at` DATETIME,
+	`name` TEXT(65535) NOT NULL,
+	`username` TEXT(65535),
+	`password` TEXT(65535),
+	`domain` TEXT(65535),
+	`two_factor_id` INTEGER,
+	`created_at` DATETIME NOT NULL,
+	`updated_at` DATETIME NOT NULL,
+	`tag_id` INTEGER,
+	PRIMARY KEY(`id`)
+);
--- testing.py
+++ testing.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def root():
     # TODO: check if the user is logged in before redirecting to login page
-    return redirect('/login')
+    return redirect("/login")
 
-@app.route('/login')
+
+@app.route("/login")
 def login_page():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
