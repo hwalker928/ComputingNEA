43e270c04441a58b381530ad26cdb6f0bd9fa020
hwalker928 <harry@harryw.link>
2024-11-29 12:03:43 +0000

    comments on the unit tests

--- encryption_test.py
+++ encryption_test.py
@@ -5,6 +5,7 @@ from encryption import *
 
 class TestEncryption(unittest.TestCase):
     def test_encrypted_is_same_as_decrypted(self):
+        """Test that an encrypted message can be decrypted."""
         message = "This is a test message."
 
         keypair = KeyPair()
@@ -18,7 +19,8 @@ class TestEncryption(unittest.TestCase):
             message,
         )
 
-    def test_encrypted_is_not_same_as_decrypted(self):
+    def test_encrypted_is_not_same_as_original(self):
+        """Test that an encrypted message is not the same as the original message."""
         message = "This is a test message."
 
         keypair = KeyPair()
@@ -28,8 +30,8 @@ class TestEncryption(unittest.TestCase):
         encrypted_data = encryption_instance.encrypt(message)
 
         self.assertNotEqual(
-            encryption_instance.decrypt(encrypted_data).decode(),
-            "This is not a test message.",
+            encrypted_data,
+            message.encode(),
         )
 
 
