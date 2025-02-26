3fee9ba08c9e94c4139dac08aebf45d8b9975641
hwalker928 <hjohnw13@gmail.com>
2025-02-26 12:42:16 +0000

    code comments woooo

--- fake_data.py
+++ fake_data.py
@@ -7,29 +7,36 @@ fake = Faker()
 
 
 def fake_data():
+    # Load the existing key pair with the generic password
     kp = encryption.KeyPair()
     kp.load_existing_key_pair("Password123!")
 
+    # Create an encryption instance
     enc = encryption.Encryption(kp)
 
+    # Connect to the SQLite database
     connection = sqlite3.connect("data/database.db")
 
-    with open("schema.sql") as f:
-        for _ in range(10):
-            connection.execute(
-                "INSERT INTO CREDENTIALS (name, username, password, domain, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
-                (
-                    fake.company(),
-                    fake.user_name(),
-                    enc.encrypt("MySecurePassword1#"),
-                    fake.domain_name(),
-                    fake.date_time_this_decade(),
-                    fake.date_time_this_decade(),
-                ),
-            )
+    # Loop 10 times to insert fake data into the database
+    for _ in range(10):
+        # Insert fake data into the database
+        connection.execute(
+            "INSERT INTO CREDENTIALS (name, username, password, domain, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
+            (
+                fake.company(),
+                fake.user_name(),
+                enc.encrypt(
+                    "MySecurePassword1#"
+                ),  # Encrypt the password before storing it in the database
+                fake.domain_name(),
+                fake.date_time_this_decade(),
+                fake.date_time_this_decade(),
+            ),
+        )
 
-        connection.commit()
-        connection.close()
+    # Commit the changes and close the connection
+    connection.commit()
+    connection.close()
 
 
 if __name__ == "__main__":
